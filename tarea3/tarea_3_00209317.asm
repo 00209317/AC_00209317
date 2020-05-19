00f71fceea641f61bc25b152337519e799be8fab
Daniel Hernandez <00209317@uca.edu.sv>
2020-05-19 12:27:02 -0600

    posicion frase 1 y mensaje 2

--- tarea3/tarea_3_00209317.asm
+++ tarea3/tarea_3_00209317.asm
@@ -1,55 +1,65 @@
-org 	100h
+	org 	100h
 
-section .text
+section 	.text
 
-	call 	texto	
-	call 	cursor
-	call 	phrase
-	call	kbwait
+		call 	texto	
+		call 	cursor
+		call 	phrase
+		call	kbwait
 
-	int 	20h
+		int 	20h
 
 texto:	mov 	ah, 00h
-	mov	al, 03h
-	int 	10h
-	ret
+		mov	al, 03h
+		int 	10h
+		ret
 
 cursor: mov	ah, 01h
-	mov 	ch, 00000000b
-	mov 	cl, 00001110b
-		;   IRGB
-	int 	10h
-	ret
+		mov 	ch, 00000000b
+		mov 	cl, 00001110b
+				;   IRGB
+		int 	10h
+		ret
 
 w_char:	mov 	ah, 09h
-	mov 	al, cl
-	mov 	bh, 0h
-	mov 	bl, 00001111b
-	mov 	cx, 1h
-	int 	10h
-	ret
+		mov 	al, cl
+		mov 	bh, 0h
+		mov 	bl, 00001111b
+		mov 	cx, 1h
+		int 	10h
+		ret
 
 kbwait: mov 	ax, 0000h
-	int 	16h
-	ret
+		int 	16h
+		ret
 
 m_cursr:mov 	ah, 02h
-	mov 	dx, di  ; columna
-	mov 	dh, 12d ; fila
-	mov 	bh, 0h
-	int 	10h
-	ret
+		mov 	dx, di  ; columna
+		mov 	dh, 3d ; fila
+		mov 	bh, 0h
+		int 	10h
+		ret
 
-phrase:	mov 	di, 29d
-lupi:	mov 	cl, [msg+di-29d]
-	call    m_cursr
-	call 	w_char
-	inc	di
-	cmp 	di, len
-	jb	lupi
-	ret
+phrase:	mov 	di, 9d
+lupi:	mov 	cl, [msg+di-9d]
+		call    m_cursr
+		call 	w_char
+		inc		di
+		cmp 	di, len
+		jb		lupi
+		ret
 
+;phrase2:mov 	di, 29d
+;lupi2:	mov 	cl, [msg+di-29d]
+;		call    m_cursr
+;		call 	w_char
+;		inc		di
+;		cmp 	di, len
+;		jb		lupi2
+;		ret
 
 section .data
-msg	db 	"Mami que tu quiere? "
+msg	    db 	"Mami que tu quiere? "
+len 	equ	$-msg+9d
+msg	    db 	"Mami que tu quiere? "
 len 	equ	$-msg+29d
